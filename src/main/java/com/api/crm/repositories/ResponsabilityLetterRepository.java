79df3d66e3dc9af987971bce49392660c13218bc
ricardomendes95 <sr_ricardomendes@hotmail.com>
2021-08-10 19:21:54 -0300

    feat: create get company for id

--- src/main/java/com/api/crm/repositories/ResponsabilityLetterRepository.java
+++ src/main/java/com/api/crm/repositories/ResponsabilityLetterRepository.java
@@ -150,6 +150,7 @@ public class ResponsabilityLetterRepository implements IResponsabilityLetterRepo
 			}
 			
 			statement.close();
+			System.out.println(companyResponsabilityLatter);
 			
 			return companyResponsabilityLatter;
 		} catch (SQLException e) {
